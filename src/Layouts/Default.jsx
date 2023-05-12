957b27111e573a1afb5fcff309a927d12f46823f
axelRubio <axelrubiomdc@gmail.com>
2023-05-12 17:24:07 -0600

    Integracion de pantalla nueva orden

--- src/Layouts/Default.jsx
+++ src/Layouts/Default.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Row, Tabs, Tab } from "react-bootstrap";
 import CatalogForms from "./CatalogForms";
+import NewOrden from "./NewOrden";
 import Toggle from "../Components/Toggle";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../CSS/NavTab.css"
@@ -13,7 +14,7 @@ const Default = () => {
             <Row className="justify-content-center m-0">
                 <Tabs justify variant="tabs" defaultActiveKey="newOrder" className="m-0 p-0">
                     <Tab eventKey="newOrder" title="Nueva Orden">
-                        Nueva Orden
+                        <NewOrden></NewOrden>
                     </Tab>
                     <Tab eventKey="report" title="Reportes">
                         Reportes
